d9295ea32f7c6018988f2aa70bf58d0539224198
azimshaik <cheer.buddu@gmail.com>
2017-02-25 11:41:48 -0500

    added event handlers

--- Spider/Form1.cs
+++ Spider/Form1.cs
@@ -26,5 +26,31 @@ namespace Spider
         {
 
         }
+
+        //event handlers
+        private void showButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
