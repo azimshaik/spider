a21a2e2f9c214de6490be13c859bd58a0b737c8b
azimshaik <cheer.buddu@gmail.com>
2017-03-07 14:49:37 -0500

    Added New form with text elemets

---
+++ Spider/Form2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using word = Microsoft.Office.Interop.Word;
+using excel = Microsoft.Office.Interop.Excel;
+using visio = Microsoft.Office.Interop.Visio;
+
+namespace Spider
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string head = HeadtextBox1.Text;
+            //MessageBox.Show(head);
+            string arm1 = Arm1textBox.Text;
+            string arm2 = Arm2textBox.Text;
+            string arm3 = Arm3textBox.Text;
+            //string[] arm1Values = arm1.Split(',');
+            //Console.WriteLine(arm1Values.Length);
+            List<string> arms = new List<string>();
+            arms.Add(arm1);
+            arms.Add(arm2);
+            arms.Add(arm3);
+            foreach (string arm in arms)
+            {
+                int i = 1;
+                string[] armValues = arm.Split(',');
+                Console.WriteLine("Length of arm"+"is"+armValues.Length);
+                i++;
+            }
+        }
+    }
+}
